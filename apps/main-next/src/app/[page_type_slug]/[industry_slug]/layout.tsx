db357f7b305d8d379adc227493a79193ca6cf76d
neolaner <neo.laner.official@gmail.com>
2026-05-16 12:45:33 +0330

    Refactor layout component; integrate PageCtxProvider and replace navigation and footer structure

--- apps/main-next/src/app/[page_type_slug]/[industry_slug]/layout.tsx
+++ apps/main-next/src/app/[page_type_slug]/[industry_slug]/layout.tsx
@@ -1,17 +1,21 @@
-import { Button } from "@workspace/ui/components/shadcn-ui/button";
 import type { ReactNode } from "react";
+import { PageCtxProvider } from "~/providers/page-ctx-provider";
+import Footer from "./_ui/footer";
+import Nav from "./_ui/nav";
 
 export default function layout({ children }: { children: ReactNode }) {
 	return (
-		<div className="flex min-h-svh w-full flex-col gap-16">
-			<nav className="flex w-full justify-between border-b-2 p-4">
-				<Button>همین الان شروع کن</Button>
-				<div className="font-bold text-2xl">Direct</div>
-			</nav>
-			<div className="flex-1">{children}</div>
-			<footer className="flex w-full justify-between bg-muted p-4 text-muted-foreground">
-				<div>© ۲۰۲۴ دایرکت. تمامی حقوق محفوظ است.</div>
-			</footer>
-		</div>
+		<PageCtxProvider>
+			<div className="flex min-h-svh w-full flex-col gap-16">
+				<Nav>
+					<Nav.Action />
+					<Nav.Brand />
+				</Nav>
+				<div className="flex-1">{children}</div>
+				<Footer>
+					<Footer.Copyright />
+				</Footer>
+			</div>
+		</PageCtxProvider>
 	);
 }
